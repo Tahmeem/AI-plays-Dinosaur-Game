d5fbcb3082a9613b406882c5d08164edcf91dfed
Tahmeem <tahmeemmahedi@gmail.com>
2021-08-02 23:23:14 -0400

    Updates to code with new images

--- src/dinosaur.py
+++ src/dinosaur.py
@@ -1,21 +1,52 @@
 import pygame
-dinoColor = 0, 0, 0
-dinoHeight = 40
-dinoWidth = 20
-class Dinosaur:
-    def __init__(self):
-        self.x = 60,
-        self.y = 0,
-        self.yVelocity = 0,
-        self.height = dinoHeight,
-        self.width = dinoWidth,
+dinocolour = 0,0,0
+DINOHEIGHT = 40
+DINOWIDTH = 20
 
-    def jump(self):
-        if self.y == 0:
-            self.yVelocity = 300
-    def update(self,timeChange):
-        self.yVelocity += -500 * timeChange
-        self.y = self.yVelocity * timeChange
-        if self.y < 0:
-            self.y = 0
-            self.yVelocity = 0
+#Images
+dinosaurImage = pygame.image.load('./Assets/Dino/DinoRun1.png')
+jumpImg = pygame.image.load('./Assets/Dino/DinoJump.png')
+duckImg = pygame.image.load('./Assets/Dino/DinoDuck1.png')
+class Dinosaur:
+  def __init__(self, surfaceHeight):
+    self.x = 60
+    self.y = 0
+
+    self.duckImg = duckImg
+    self.jumping = jumpImg
+
+    self.duck = False
+    self.jumpBool = False
+    self.run = True
+    self.Img = dinosaurImage
+
+
+    self.yvelocity = 0
+    self.height = DINOHEIGHT
+    self.width = DINOWIDTH
+    self.surfaceHeight = surfaceHeight
+  def jump(self):
+    if(self.y == 0):
+      self.yvelocity = 300
+  def update(self, userInput):
+    if self.jumpBool:
+      self.jump()
+    if self.duck:
+      self.duck()
+    if self.run:
+      self.run()
+    if userInput[pygame.K_SPACE] and not self.jumpBool:
+      self.duck = False
+      self.jumpBool = True
+      self.run = False
+    elif userInput[pygame.K_DOWN] and not self.jumpBool:
+      self.duck = True
+      self.jumpBool = False
+      self.run = False
+    elif not (self.jumpBool or userInput[pygame.K_DOWN]):
+      self.duck = False
+      self.jumpBool = False
+      self.run = True
+
+  def draw(self,display):
+    pygame.draw.rect(display,dinocolour,[self.x,self.surfaceHeight-self.y-self.height,self.width,self.height])
--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
 import pygame
+from dinosaur import Dinosaur
 pygame.init()
 
-size = width, height = 840, 680
+size = width, height = 640, 480
 display = pygame.display.set_mode(size)
-cactusImage = pygame.image.load('../Assets/Cactus.png')
-groundHeight = height - 400
-xPos = 0
-yPos = 0
+
+groundHeight = height - 100
+dinosaur = Dinosaur(groundHeight)
+
+#Images
+cactus = pygame.image.load('./Assets/Cactus.png')
+bird = pygame.image.load('./Assets/Bird1.png')
+
+
+lastFrame = pygame.time.get_ticks()
 
 #Colors
 grey = 169, 169, 169
@@ -14,13 +21,19 @@ black = 0, 0, 0
 white = 255, 255, 255
 
 while True:
+    curr_t = pygame.time.get_ticks()
+    timeChange = (curr_t-lastFrame)/1000.0
+    lastFrame = curr_t
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-    #display.blit(cactusImage, (50, 50))
-    display.fill(grey)
-    pygame.draw.rect(display, black, [0, 400, width, 20])
-    xPos += 1
-    yPos += 1
+        if event.type == pygame.KEYDOWN: #If keyboard used
+            if event.type == pygame.K_SPACE:
+                Dinosaur.jump()
+
+    display.fill(white)
+    dinosaur.update(timeChange)
+    dinosaur.draw(display)
+    pygame.draw.rect(display, black, [0, groundHeight, width, 20])
     pygame.display.update()
