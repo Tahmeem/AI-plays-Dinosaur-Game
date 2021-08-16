cfe863c3665eaff1a85b4b290b83412562eb0e26
Tahmeem <tahmeemmahedi@gmail.com>
2021-08-15 20:56:17 -0400

    agent modified

--- src/agent.py
+++ src/agent.py
@@ -13,3 +13,49 @@ class Agent:
         self.epsilon = 0  # randomness
         self.gamma = 0.9  # discount rate
         self.memory = deque(maxlen=MAX_MEMORY)
+
+    def get_State(self,game):
+        pass
+
+    def remember(self,state,action,reward,nextState,gameOver):
+        pass
+
+    def trainLongMemory(self):
+        pass
+
+    def traingShortMemory(self,state,action,reward,nextState,gameOver):
+        pass
+
+    def getAction(self,state):
+        pass
+
+def train():
+    scores = []
+    meanScores = []
+    total = 0
+    highScore = 0
+    agent = Agent()
+    newGame = game()
+    while True:
+        oldState = agent.get_State(newGame)
+
+        lastMove = agent.getAction(oldState)
+
+        reward, gameOver, score = newGame.playing(lastMove)
+
+        newState = agent.get_State(newGame)
+
+        agent.traingShortMemory(oldState,lastMove,reward,newState,gameOver)
+
+        agent.remember(oldState,lastMove,reward,newState,gameOver)
+
+        if gameOver:
+            newGame.reset()
+            agent.n_games += 1
+            agent.trainLongMemory()
+
+            if score > highScore:
+                highScore = score
+            print(f'Game {agent.n_games} score {score} High Score {highScore}')
+if __name__ == '__main__':
+    train()
--- src/main.py
+++ src/main.py
@@ -23,7 +23,6 @@ black = 0, 0, 0
 white = 255, 255, 255
 
 
-clock = pygame.time.Clock()
 obstacles = []
 
 
@@ -34,50 +33,61 @@ class game:
         self.gameOver = False
         self.score = 0
         self.display = pygame.display.set_mode(size)
+        self.clock = pygame.time.Clock()
+        self.reset()
 
+    def reset(self):
+        self.score = 0
+        obstacles = []
 
-        def playing(self,action):
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
+    def collision(self,obstacle):
+        if dinosaur.dinoRect.colliderect(obstacle.imageRect):
+            return True
+        return False
 
-            self.display.fill(white)
-            #userInput = pygame.key.get_pressed()
+    def playing(self,action):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
-            dinosaur.draw(self.display)
-            dinosaur.update(action)
+        self.display.fill(white)
+        #userInput = pygame.key.get_pressed()
 
-            if len(obstacles) == 0:
-                randomNumber = random.randint(0,1)
-                if randomNumber == 0:
-                    obstacles.append(Cactus(cactus))
-                elif randomNumber == 1:
-                    obstacles.append(Bird(bird))
+        dinosaur.draw(self.display)
+        dinosaur.update(action)
 
-            for obstacle in obstacles:
-                obstacle.draw(self.display)
-                obstacle.update(obstacles)
-                if dinosaur.dinoRect.colliderect(obstacle.imageRect):
-                    self.reward -= 10
-                    self.gameOver = True
-                    if self.score > self.highScore:
-                        self.highScore = self.score
-                    game(self.highScore)
+        if len(obstacles) == 0:
+            randomNumber = random.randint(0,1)
+            if randomNumber == 0:
+                obstacles.append(Cactus(cactus))
+            elif randomNumber == 1:
+                obstacles.append(Bird(bird))
 
-            score = pointFunc(self.score,self.reward)[0]
-            reward = score
-            font = pygame.font.SysFont(None, 24)
-            scoreReader = font.render(f"Score: {score}", True, (0, 0, 0))
-            scoreRect = scoreReader.get_rect()
-            scoreRect.center = (550, 40)
-            self.display.blit(scoreReader, scoreRect)
-            highScoreReader = font.render(f"High: {highScore}", True, (0, 0, 0))
-            highScoreRect = highScoreReader.get_rect()
-            highScoreRect.center = (460, 40)
-            self.display.blit(highScoreReader, highScoreRect)
+        for obstacle in obstacles:
+            obstacle.draw(self.display)
+            obstacle.update(obstacles)
+            if self.collision(obstacle):
+                self.reward -= 10
+                self.gameOver = True
+                if self.score > self.highScore:
+                    self.highScore = self.score
+                return self.reward,self.gameOver,self.score
 
-            pygame.draw.rect(self.display, black, [0, groundHeight, width, 20])
+        score = pointFunc(self.score,self.reward)[0]
+        reward = score
+        font = pygame.font.SysFont(None, 24)
+        scoreReader = font.render(f"Score: {score}", True, (0, 0, 0))
+        scoreRect = scoreReader.get_rect()
+        scoreRect.center = (550, 40)
+        self.display.blit(scoreReader, scoreRect)
+        highScoreReader = font.render(f"High: {self.highScore}", True, (0, 0, 0))
+        highScoreRect = highScoreReader.get_rect()
+        highScoreRect.center = (460, 40)
+        self.display.blit(highScoreReader, highScoreRect)
 
-            clock.tick(30)
-            pygame.display.update()
+        pygame.draw.rect(self.display, black, [0, groundHeight, width, 20])
+
+        self.clock.tick(30)
+        pygame.display.update()
+        return self.reward, self.gameOver, self.score
